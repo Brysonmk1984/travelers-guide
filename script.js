9531e67367863253c1cf032086c6acc7b3fc8bb9
brysonmk1984 <brysonmk1984@gmail.com>
2014-06-15 19:27:36 -0600

    Location and weather are working but not together

--- script.js
+++ script.js
@@ -1,39 +1,87 @@
-$(document).ready(function(){
-	var show_map = function(position){
-		var lat = position.coords.latitude;
-		var lon = position.coords.longitude;
-		//console.log(lat);console.log(lon);
-		$.get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=4ba34577dab3de03fbe0b53eade625dea3ce8cdf&format=json&q="+lat+","+lon, function(data){
-			//console.log(data);
-			// $(data.weather.0).each(function(){
-			// 	console.log('hi');
-			// });
-		// console.log(data.data.weather[0]);
-		// for(item in data.data.weather[0]){
-		// 	var key = item;
-		// 	var value = data.data.weather[0][item];
-		// 	$("#weatherList").append("<li>"+item +":"+value+"</li>");
-		// }
+// $(document).ready(function(){
+// 	var show_map = function(position){
+// 		var lat = position.coords.latitude;
+// 		var lon = position.coords.longitude;
+// 		console.log(lat);console.log(lon);
+// 		$.get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=4ba34577dab3de03fbe0b53eade625dea3ce8cdf&format=json&q="+lat+","+lon, function(data){
 			
-		});
-	};
-	var get_location = function() {
-	  navigator.geolocation.getCurrentPosition(show_map);
-	};
-	get_location();
+// 		for(item in data.data.weather[0]){
+// 			var key = item;
+// 			var value = data.data.weather[0][item];
+// 			$("#weatherList").append("<li>"+item +":"+value+"</li>");
+// 		}
+			
+// 		});
+// 	};
+// 	var get_location = function() {
+// 	  navigator.geolocation.getCurrentPosition(show_map);
+// 	};
+// 	get_location();
 
 
-(function(){
-	var weatherModule = angular.module('weather',[]);
-	
-	weatherModule.controller('WeatherController',['$http',function($http){
-		$http.get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=4ba34577dab3de03fbe0b53eade625dea3ce8cdf&format=json&q="+lat+","+lon)
-			.success(function(data){
-		    console.log(data);
-		    for(item in data.data.weather[0]){
-		    	
-		    }
-		});
-	}]);
-})();
+
+// });
+
+var travelersApp = angular.module('travelersApp', []);
+
+travelersApp.factory('weatherFactory', function($http){
+	var factory = {};
+
+	factory.findLocation = function(){
+		var locationObject = {};
+		var myLatLon = function(position){
+			return {
+				lat : position.coords.latitude,
+				lon : position.coords.longitude
+			}
+		};
+		function geoSuccess(p) {
+		  alert("Found you at latitude " + p.coords.latitude +
+		        ", longitude " + p.coords.longitude);
+			locationObject = {
+				lat : p.coords.latitude,
+				lon : p.coords.longitude
+			}
+		}
+		function geoError(p) {
+		  alert("can't get location");
+		}
+		
+		navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+
+		return {
+			lat : 40,
+			lon : -105
+		}
+	};
+
+	factory.getWeatherData = function(){
+		var myLocation = factory.findLocation();
+		return $http.get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=4ba34577dab3de03fbe0b53eade625dea3ce8cdf&format=json&q="+myLocation.lat+","+myLocation.lon);
+	};
+	return factory.getWeatherData();
 });
+
+travelersApp.controller('WeatherCtrl',['$scope', 'weatherFactory', function($scope, weatherFactory){
+
+		$scope.weatherHeadline = "The weather today will be:";
+		$scope.handleWeatherData = function(data){
+			$scope.weather = data.data.weather[0];
+			console.log($scope.weather);
+		};
+		$scope.weather = weatherFactory.success($scope.handleWeatherData);
+	}
+]);
+
+
+
+	
+
+
+
+/*travelersApp.controller('WeatherDataCtrl',['weatherFactory',
+	function(weatherFactory){
+		this.message = weatherFactory;
+		console.log(this.message);
+	}
+]);*/
