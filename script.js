dd76c35150145014f1f5968ffebddc5da4117e08
unknown <brysonmk1984@gmail.com>
2014-06-17 19:53:29 -0600

    got the weather syncing with location, got map started

--- script.js
+++ script.js
@@ -1,50 +1,78 @@
 var travelersApp = angular.module('travelersApp', []);
 
-var locationObject = {};
-var findMyLocation = function(){
-	function geoSuccess(p) {
-	  alert("Found you at latitude " + p.coords.latitude +
-	        ", longitude " + p.coords.longitude);
-		locationObject = {
-			lat : p.coords.latitude,
-			lon : p.coords.longitude
-		}
-	}
-	function geoError(p) {
-	  alert("can't get location");
-	}
-	navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-};
-findMyLocation();
-
-
-travelersApp.factory('weatherFactory', function($http){
+/*travelersApp.factory('weatherFactory', function($http){
 	var factory = {};
-	factory.getWeatherData = function(){
-		var myLocation = {
-			lat : 40,
-			lon : -105
-		};
-		return $http.get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=4ba34577dab3de03fbe0b53eade625dea3ce8cdf&format=json&q="+myLocation.lat+","+myLocation.lon);
+	factory.getWeatherData = function(){	
 	};
 	return factory.getWeatherData();
-});
+});*/
 
-travelersApp.controller('WeatherCtrl',['$scope', 'weatherFactory', function($scope, weatherFactory){
-
-		// setTimeout(function(){
-
-		// 	weatherFactory();
-		// },7000);
-		
-
+travelersApp.controller('WeatherCtrl',['$scope', '$http', function($scope, $http){
 
 		$scope.weatherHeadline = "The weather today will be:";
-		$scope.handleWeatherData = function(data){
-			$scope.weather = data.data.weather[0];
-			console.log($scope.weather);
+		$scope.weather;
+		$scope.handleWeatherData = function(weatherData){
+			$scope.weather = weatherData.data.weather[0];
+			console.log(weatherData.data.weather[0]);
+
 		};
-		// $scope.weather = weatherFactory.success($scope.handleWeatherData);
+		var locationObject = {};
+		var findMyLocation = function(){
+			function geoSuccess(p) {
+			  console.log("Found you at latitude " + p.coords.latitude +
+			        ", longitude " + p.coords.longitude);
+				locationObject = {
+					lat : p.coords.latitude,
+					lon : p.coords.longitude
+				};
+
+
+
+				function initialize() {
+				  var mapOptions = {
+				    center: new google.maps.LatLng(locationObject.lat, locationObject.lon),
+				    zoom: 12
+				  };
+				  var map = new google.maps.Map(document.getElementById("map-canvas"),
+				      mapOptions);
+				}
+				initialize();
+
+
+				/*geocoder.geocode({'latLng': latlng}, function(results, status) {
+				   if (status == google.maps.GeocoderStatus.OK) {
+				     if (results[1]) {
+				       map.setZoom(11);
+				       marker = new google.maps.Marker({
+				           position: latlng,
+				           map: map
+				       });
+				       infowindow.setContent(results[1].formatted_address);
+				       infowindow.open(map, marker);
+				     } else {
+				       alert('No results found');
+				     }
+				   } else {
+				     alert('Geocoder failed due to: ' + status);
+				   }
+				 });*/
+
+
+
+				$scope.weather = $http.get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=4ba34577dab3de03fbe0b53eade625dea3ce8cdf&format=json&q="+locationObject.lat+","+locationObject.lon)
+				.success($scope.handleWeatherData);
+			}
+			function geoError(p) {
+			  alert("can't get location");
+			}
+			navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+		};
+		findMyLocation();
+
+
+		
+		
+
 	}
 ]);
 
