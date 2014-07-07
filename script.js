c2b6a6a1a73699a782ebb25df287c9084ca46fb1
brysonmk1984 <brysonmk1984@gmail.com>
2014-07-06 18:59:44 -0600

    got the dates show up consistantly, no more double load

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 var travelersApp = angular.module('travelersApp', []);
-
-/*travelersApp.factory('weatherFactory', function($http){
-	var factory = {};
-	factory.getWeatherData = function(){	
-	};
-	return factory.getWeatherData();
-});*/
-
 travelersApp.controller('WeatherCtrl',['$scope', '$http', function($scope, $http){
-
 		$scope.weatherHeadline = "The weather today will be:";
 		$scope.weather;
 		$scope.currentLocation;
 		$scope.city;
 		$scope.state;
-		$scope.lat;
-		$scope.lng;
+		$scope.country;
 		$scope.handleWeatherData = function(weatherData){
 			$scope.weather = weatherData.data.weather[0];
-			console.log(weatherData.data.weather[0]);
+			//console.log(weatherData.data.weather[0]);
 
 		};
 		var locationObject = {};
@@ -31,64 +21,41 @@ travelersApp.controller('WeatherCtrl',['$scope', '$http', function($scope, $http
 				
 				function initializeMap() {
 				   
-				    var mapOptions = {
-				       center: new google.maps.LatLng(locationObject.lat, locationObject.lng),
-				       zoom: 12
-				    };
-				    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-				    geocoder = new google.maps.Geocoder();
+					var mapOptions = {
+					   center: new google.maps.LatLng(locationObject.lat, locationObject.lng),
+					   zoom: 12
+					};
+					var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+					geocoder = new google.maps.Geocoder();
 				 }
 				initializeMap(); 
 
 				function codeLatLng(lat, lng) {
-				    var latlng = new google.maps.LatLng(lat, lng);
-				    geocoder.geocode({'latLng': latlng}, function(results, status) {
-				      if (status == google.maps.GeocoderStatus.OK) {
-				        if (results[1]) {
-				        	console.log(results);
-				        //find country name
-				             for (var i=0; i<results[0].address_components.length; i++) {
-				            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+					var latlng = new google.maps.LatLng(lat, lng);
+					geocoder.geocode({'latLng': latlng}, function(results, status) {
+						$scope.$apply( function () {
+							  if (status == google.maps.GeocoderStatus.OK) {
+								if (results[1]) {
+									console.log(results[1]);
+									$scope.currentLocation = results[1].formatted_address;
+									$scope.city = results[1].address_components[1].long_name;
+									$scope.state = results[1].address_components[2].short_name;
+									$scope.country = results[1].address_components[3].short_name;
 
-				            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-				                if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-				                    //this is the object you are looking for
-				                    city= results[0].address_components[i];
-				                    break;
-				                }
-				            }
-				        }
-				        
-				        
 
-				        	$scope.currentLocation = results[0].formatted_address;
-				        	$scope.city = results[0].address_components[3].long_name;
-				        	$scope.state = results[0].address_components[5].short_name;
-				        	$scope.lat = locationObject.lat;
-				        	$scope.lng = locationObject.lng;
 
-				        } else {
-				          alert("No results found");
-				        }
-				      } else {
-				        alert("Geocoder failed due to: " + status);
-				      }
-				    });
+								} else {
+								  alert("No results found");
+								}
+							  } else {
+								alert("Geocoder failed due to: " + status);
+							  }
+						});
+					});
 				}
-
-
-
-				codeLatLng(locationObject.lat, locationObject.lng);
 				
-
-  				
-
-			
-
-
-
-
-
+					$scope.weatherResults = codeLatLng(locationObject.lat, locationObject.lng);
+	
 				$scope.weather = $http.get("http://api.worldweatheronline.com/free/v1/weather.ashx?key=4ba34577dab3de03fbe0b53eade625dea3ce8cdf&format=json&q="+locationObject.lat+","+locationObject.lng)
 				.success($scope.handleWeatherData);
 			}
@@ -99,9 +66,6 @@ travelersApp.controller('WeatherCtrl',['$scope', '$http', function($scope, $http
 		};
 		findMyLocation();
 
-
-		
-		
 
 	}
 ]);
